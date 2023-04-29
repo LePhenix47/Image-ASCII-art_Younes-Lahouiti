8bb23725ecc87e1aa46616b88647c4ddc07c651a
Younes Lahouiti <loller763@gmail.com>
2023-04-29 14:43:19 +0200

    Initial commit

---
+++ src/utils/functions/number.functions.ts
@@ -0,0 +1,36 @@
+/**
+ * Generates a random number within a specified range.
+ * @param {number} min - The minimum value of the range.
+ * @param {number} max - The maximum value of the range.
+ * @param {boolean} includeMin - Whether to include the minimum value in the range.
+ * @param {boolean} includeMax - Whether to include the maximum value in the range.
+ *
+ * @returns {number} A random number within the specified range.
+ */
+function getRandomNumber(
+  min: number,
+  max: number,
+  includeMin: boolean = true,
+  includeMax: boolean = true
+): number {
+  const hasInvalidArgument: boolean = min > max || max < min;
+  if (hasInvalidArgument) {
+    throw new Error(`Unexpected error occured values: min > max or max < min`);
+  }
+
+  const mustIncludeBoth: boolean = includeMin && includeMax;
+
+  const mustIncludeOnlyMin: boolean = includeMin && !includeMax;
+
+  const mustIncludeOnlyMax: boolean = !includeMin && includeMax;
+
+  if (mustIncludeBoth) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else if (mustIncludeOnlyMin) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  } else if (mustIncludeOnlyMax) {
+    return Math.floor(Math.random() * (max - min)) + min + 1;
+  } else {
+    return Math.floor(Math.random() * (max - min - 1)) + min + 1;
+  }
+}
